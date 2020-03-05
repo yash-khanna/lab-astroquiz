8ebb4896de3fa9476a365e1415e4833e2aab0df2
FACEPrep-ProGrad <61002120+FACEPrep-ProGrad@users.noreply.github.com>
2020-03-05 14:08:04 +0530

    Update EligibiltyCheck.java

--- src/utility/EligibiltyCheck.java
+++ src/utility/EligibiltyCheck.java
@@ -1,31 +1,5 @@
 package utility;
 
-import model.User;
-
-public class EligibiltyCheck extends BasicEligibility implements EligibilityInterface{
-
-
-	
-	public boolean checkQuizAnswer(String points) {
-		
-		return true;
-	}
-
-	public boolean checkUser(User user) {
-		
-		return true;
-	}
-	public boolean basicEligibilityCheck(User user) {
-		if(checkUser(user))
-			return true;
-		else
-		return false;
-	}
-
-
-	
-
-}
 
 
 
