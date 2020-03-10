492de9fd76d5dc09815351edb3b099e837d44cd9
FACEPrep-ProGrad <61002120+FACEPrep-ProGrad@users.noreply.github.com>
2020-03-10 21:08:00 +0530

    Update EligibilityViewController.java

--- src/controller/EligibilityViewController.java
+++ src/controller/EligibilityViewController.java
@@ -45,11 +45,6 @@ public class EligibilityViewController extends HttpServlet {
 		user.setAge(age);
 		user.setHeight(height);
 		user.setWeight(weight);
-		user.setCountry(country);
-		// Create an Object for EligibiltyCheck
-		// Call the basicEligibilityCheck method with the object created pass the user argument as the value to method.
-		// Store the return value in a boolean variable called spaceEligible
-		 
 		
 		System.out.println(spaceEligible);
 			if(spaceEligible)
